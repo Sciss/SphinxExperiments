1cd6d29717cefd272c6c84baa30ea60a6e3250d1
Hanns Holger Rutz <contact@sciss.de>
2016-02-19 00:12:02 +0100

    init

---
+++ build.sbt
@@ -0,0 +1,8 @@
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+  "de.sciss" %  "sphinx4-core"      % "1.0.0",
+  "de.sciss" %  "sphinx4-data"      % "1.0.0",
+  "de.sciss" %% "fileutil"          % "1.1.1",
+  "de.sciss" %% "audiowidgets-core" % "1.9.1"
+)
---
+++ src/main/scala/de/sciss/sphinxex/Test1.scala
@@ -0,0 +1,55 @@
+package de.sciss.sphinxex
+
+import java.io.FileInputStream
+
+import de.sciss.audiowidgets.AxisFormat
+import de.sciss.file._
+import edu.cmu.sphinx.api.{Configuration, StreamSpeechRecognizer}
+
+import scala.annotation.tailrec
+import scala.collection.JavaConverters
+
+// -DlogLevel=WARNING
+object Test1 extends App {
+  val cfg = new Configuration
+
+  cfg.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us")
+  cfg.setDictionaryPath   ("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict")
+  cfg.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin")
+
+  val rec   = new StreamSpeechRecognizer(cfg)
+  val fIn   = userHome / "Downloads" / "Big_Ego_12-burroughsRsmp.wav"
+  val is    = new FileInputStream(fIn)
+
+  rec.startRecognition(is)
+
+  val SampleRate  = 16000.0 // sample rate must be 16K, or otherwise funny things happen
+  val BlockSize   = 16      // XXX TODO -- is this fixed?
+  val timeFmt     = AxisFormat.Time()
+
+  @tailrec def loop(): Unit = Option(rec.getResult) match {
+    case Some(speechRes) =>
+      val res   = speechRes.getResult
+      // val hypo  = res.getBestResultNoFiller
+      res.getCollectTime
+      res.getStartFrame
+      import JavaConverters._
+      val list = res.getTimedBestResult(true).asScala
+      val listS = list.map { wordRes =>
+        val word    = wordRes.getWord
+        val frame   = wordRes.getTimeFrame
+        val start   = frame.getStart * BlockSize / SampleRate
+        val end     = frame.getEnd   * BlockSize / SampleRate
+        val startF  = timeFmt.format(start)
+        val endF    = timeFmt.format(end  )
+        // s"{${word.word}, %.3f, [%s]}", word, LogMath.getLogMath().logToLinear((float)getConfidence()), timeFrame);
+        s"{$word, [$startF - $endF]}"
+      }
+      println(listS.mkString(", "))
+      loop()
+    case None =>
+      rec.stopRecognition()
+      println("Done.")
+  }
+  loop()
+}
